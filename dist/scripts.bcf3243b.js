23317fad8220992d4f7cd0a1c342b937973c8f41
Angel Rafael Rodriguez Hernandez <angel01rdz@outlook.com>
2022-04-28 11:20:49 -0500

    Mobile bug fixes

--- dist/scripts.bcf3243b.js
+++ dist/scripts.bcf3243b.js
@@ -9054,6 +9054,9 @@ var scroll = new _locomotiveScroll.default({
   smooth: true,
   tablet: {
     smooth: true
+  },
+  smartphone: {
+    smooth: true
   }
 });
 
@@ -9139,7 +9142,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51784" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57723" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
